ff78e204324123312905d0337e98ff5b8c4ffe93
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-07-12 17:07:28 +0900

    add edit link

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,6 +2,7 @@
 
 @foreach($posts as $post)
     <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+    <a href="/posts/{{ $post->id }}/edit">Edit</a>
 @endforeach
 
 <a href="/posts/create">New Post</a> 
